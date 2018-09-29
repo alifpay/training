7c721f6935ebe07fce083e3266de3eb8ad6f7938
Alif.pay <42164453+alifpay@users.noreply.github.com>
2018-09-29 12:27:09 +0500

    Update utils.go

--- testing/utils.go
+++ testing/utils.go
@@ -23,23 +23,23 @@ func BeginningOfDay(dt time.Time) time.Time {
 
 
 //SomoniToInt convert somoni to int
+// return 0 means could not convert
+// it is not validate is it zero
 func SomoniToInt(amt string) int64 {
 	amt = strings.Replace(amt, ",", ".", -1)
 	amts := strings.Split(amt, ".")
 	if len(amts) == 1 {
-		if s, err := strconv.ParseInt(amt, 10, 64); err == nil {
-			return s * 100
-		}
-	} else {
-		if len(amts[1]) == 1 {
-			amts[1] = amts[1] + "0"
-		} else if len(amts[1]) > 2 {
-			amts[1] = amts[1][:2]
-		}
-		amt = amts[0] + amts[1]
-		if s, err := strconv.ParseInt(amt, 10, 64); err == nil {
-			return s
-		}
+		s, _ := strconv.ParseInt(amt, 10, 64)
+		return s * 100
 	}
-	return 0
+	if ln := len(amts[1]); ln == 1 {
+		amts[1] = amts[1] + "0"
+	} else if ln > 2 {
+		amts[1] = amts[1][:2]
+	} else if ln == 0 {
+		amts[1] = amts[1] + "00"
+	}
+	amt = amts[0] + amts[1]
+	s, _ := strconv.ParseInt(amt, 10, 64)
+	return s
 }
